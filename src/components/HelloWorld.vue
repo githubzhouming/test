6f4de3706b7c7cb1a4aeadaf88786a644265f58c
githubzhouming <799836942@qq.com>
2022-03-17 10:33:09 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { ref ,computed} from 'vue'
+import {useStore} from '../store/index'
+
+defineProps<{ msg: string }>()
+const store=useStore()
+const showCount = computed(()=>{
+  return store.getters['getCount']
+})
+const count=ref(0)
+const addBtn=()=>{
+  store.commit('setCount',++count.value)
+}
+</script>
+
+<template>
+  <h1>{{ showCount }}</h1>
+
+  <button type="button" @click="addBtn">新增</button>
+  
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router/index'
+import {store,key} from './store/index'
+createApp(App).use(router).use(store,key).mount('#app')
